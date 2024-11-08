6b03b2ea56934de63bcae7f4387076f0840a2684
Nick <150384234+njindra03@users.noreply.github.com>
2024-11-07 19:47:30 -0500

    milestone3

--- Dungeon Master/Assets/Scripts/GameManager.cs
+++ Dungeon Master/Assets/Scripts/GameManager.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // When the player touches the hatch tilemap, load the next scene (Room1)
+    // When the player touches the hatch tilemap, load the next scene randomly but not the current scene and never the spawnroom
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene("Room1");
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = Random.Range(1, SceneManager.sceneCountInBuildSettings);
+            while (nextSceneIndex == sceneIndex || nextSceneIndex == 0)
+            {
+                nextSceneIndex = Random.Range(1, SceneManager.sceneCountInBuildSettings);
+            }
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
     //When the player presses the tab key, load the shop menu scene
